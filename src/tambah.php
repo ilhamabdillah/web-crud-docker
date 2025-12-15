ae7565e5c3e54a4da30a0fc1fd10857a5e3ecb47
ilhamabdillah <nebulaa1322@gmail.com>
2025-12-15 23:29:47 +0700

    selesai sudah

--- src/tambah.php
+++ src/tambah.php
@@ -1,4 +1,14 @@
-<?php include 'config.php'; ?>
+<?php
+include 'config.php';
+
+if ($_POST) {
+  mysqli_query($koneksi,
+    "INSERT INTO mahasiswa (nama, nim)
+     VALUES ('$_POST[nama]', '$_POST[nim]')");
+  header("Location: index.php");
+  exit;
+}
+?>
 
 <h2>Tambah Mahasiswa</h2>
 
@@ -7,12 +17,3 @@ Nama: <input type="text" name="nama" required><br><br>
 NIM: <input type="text" name="nim" required><br><br>
 <button type="submit">Simpan</button>
 </form>
-
-<?php
-if ($_POST) {
-  mysqli_query($koneksi,
-    "INSERT INTO mahasiswa (nama, nim)
-     VALUES ('$_POST[nama]', '$_POST[nim]')");
-  header("Location: index.php");
-}
-?>
