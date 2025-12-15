d83ea0be02c395f3236fc9c0cf6a34aa16594668
ilhamabdillah <nebulaa1322@gmail.com>
2025-12-15 22:52:53 +0700

    UPDATE

--- src/tambah.php
+++ src/tambah.php
@@ -4,15 +4,15 @@
 
 <form method="post">
 Nama: <input type="text" name="nama" required><br><br>
-NIM: <input type="text" name="nim" required><br><br>
+NIM: <input type="text" name="NPM" required><br><br>
 <button type="submit">Simpan</button>
 </form>
 
 <?php
 if ($_POST) {
   mysqli_query($koneksi,
-    "INSERT INTO mahasiswa (nama, nim)
-     VALUES ('$_POST[nama]', '$_POST[nim]')");
+    "INSERT INTO mahasiswa (nama, NPM)
+     VALUES ('$_POST[nama]', '$_POST[NPM]')");
   header("Location: index.php");
 }
 ?>
